553336d0b4636cb20f02840b27b69e41e42ed695
larntech <larntech20@gmail.com>
2021-06-12 12:56:53 +0300

    first commit

---
+++ app/src/main/java/net/larntech/navdrawer/ui/logout/LogoutFragment.kt
@@ -0,0 +1,25 @@
+package net.larntech.navdrawer.ui.logout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import net.larntech.navdrawer.R
+import net.larntech.navdrawer.ui.home.HomeViewModel
+
+class LogoutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_logout, container, false)
+
+        return root
+    }
+}
